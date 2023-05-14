ab38b854f6240bd29d8288b1a710732f1d7d2f41
davidgzdeveloper <davidgzdeveloper@gmail.com>
2023-05-13 22:13:28 -0300

    images added

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 const router = Router()
 
-router.get('/', (req, res) => res.render('index', {title: 'First web with Node'}))
+router.get('/', (req, res) => res.render('index', {title: 'Portfolio'}))
 router.get('/about', (req, res) => res.render('about', {title: 'About Me'}))
 router.get('/contact', (req, res) => res.render('contact', {title: 'Contact page'}))
 router.get('/projects', (req, res)=> res.render('projects', {title: 'Projects'}))
